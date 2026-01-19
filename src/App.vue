13710bfb299ee1b5da655505c6bdd6c5c50b2865
Kim Dudenhöfer <kidu2400@student.miun.se>
2026-01-19 13:24:17 +0100

    Skapat responsiv navigation.

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,23 @@
 </script>
 
 <template>
+
+  <!-- Logo on sm -->
+  <div class="position-relative d-md-none">
+    <img src="/src/router/assets/logo.svg" class="position-absolute top-0 end-0 p-2 z-3" width="90" alt="Logo">
+  </div>
+
   <!-- Grid -->
   <div class="container-fluid">
 
-    <div class="row p-0">
+    <div class="row">
       <!-- Sidebar with nav -->
-      <div class="col-2 p-0 vh-100">
-        <Navigation />
+      <div class="col-12 col-md-3 col-lg-2 order-2 order-md-1 p-0">
+        <Navigation class="pb-md-4"/>
       </div>
 
       <!-- Main content -->
-      <main class="col-10 p-4">
+      <main class="col col-md-9 col-lg-10 order-1 order-md-2 p-4">
         <RouterView />
       </main>
 
--- src/router/components/Navigation.vue
+++ src/router/components/Navigation.vue
@@ -1,11 +1,46 @@
 <template>
+    <!-- Navigation -->
+    <nav class="navbar bg-info justify-content-center">
 
+        <!-- Logo -->
+        <RouterLink to="/" class="navbar-brand d-none d-md-block mb-4 mt-4">
+            <img src="../assets/logo.svg" width="130" alt="Logo">
+        </RouterLink>
+
+        <!-- Main navigation -->
+         <div class="container justify-content-center mt-0 mt-md-4">
+            <ul class="navbar-nav nav-md-none flex-row flex-md-column justify-content-between w-75">  
+                <li class="nav-item d-flex py-2 px-4 px-md-0">
+                    <RouterLink to="/" class="nav-link d-flex flex-column flex-md-row align-items-center align-self-center" title="Till kontrollpanel">
+                        <img src="../assets/dashboard_icon.svg" class="pe-md-2" width="30" alt="Lager">
+                        <span class="d-none d-sm-flex">Kontrollpanel</span>
+                    </RouterLink>
+                </li>
+                <li class="nav-item d-flex py-2 px-4 px-md-0">
+                    <RouterLink to="/products" class="nav-link d-flex flex-column flex-md-row align-items-center align-self-center" title="Till lager">
+                        <img src="../assets/store_icon.svg" class="pe-md-2" width="30" alt="Lager">
+                        <span class="d-none d-sm-flex">Lager</span>
+                    </RouterLink>
+                </li>
+                <li class="nav-item d-flex py-2 px-4 px-md-0">
+                    <RouterLink to="/user" class="nav-link d-flex flex-column flex-md-row align-items-center align-self-center" title="Till användare">
+                        <img src="../assets/user_icon.svg" class="pe-md-2" width="30" alt="Lager">
+                        <span class="d-none d-sm-flex">Användare</span>
+                    </RouterLink>
+                </li>
+            </ul>
+         </div>
+    </nav>
 </template>
 
 <script setup>
+    import { RouterLink } from 'vue-router';
 
 </script>
 
 <style scoped>
-    
+    /* Links */
+    .router-link-active {
+        font-weight: 700;
+    }
 </style>
